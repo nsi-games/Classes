197f7c1c553606e913c39749eeb7800f81305049
Manny Vaccaro <emmanuel.vaccaro@gmail.com>
2020-03-06 13:50:32 +1100

    Initial Commit

---
+++ Assets/Common/Scripts/MouseOrbit.cs
@@ -0,0 +1,83 @@
+﻿/* Author: Emmanuel (Manny) Vaccaro
+ * Date: 05/03/2020
+ * Brief: Component for controlling Camera Orbit at Runtime
+ */
+
+using UnityEngine;
+
+[AddComponentMenu("Camera Control/Mouse Orbit")] // Adds this script to the 'Add Component' Menu under 'Camera Control'
+public class MouseOrbit : MonoBehaviour
+{
+    #region Variables
+    public bool hideCursor = true;      // Should the Cursor be hidden?
+    public Transform target;            // Defined Target (defaults to zero if not referencing anything)
+    public float yawSpeed = 120.0f;     // Speed of Yaw (Degrees)
+    public float pitchSpeed = 120.0f;   // Speed of Pitch (Degrees)
+    public float pitchMinLimit = -20f;  // Min Pitch Limit (Degrees)
+    public float pitchMaxLimit = 80f;   // Max Pitch Limit (Degrees)
+    public float distanceMin = .5f;     // Min Distance (Units)
+    public float distanceMax = 15f;     // Max Distance (Units)
+    private float distance = 5.0f;      // Current Distance (Units)
+    private float yaw = 0.0f;           // Current Yaw (Degrees)
+    private float pitch = 0.0f;         // Current Pitch (Degrees)
+    #endregion
+    #region Functions/Methods
+    // Use this for initialization
+    private void Start()
+    {
+        // Store the current Yaw (Y) & Pitch (X) of the Camera. No need for Roll (Z)
+        Vector3 angles = transform.eulerAngles;
+        yaw = angles.y;
+        pitch = angles.x;
+
+        // Hide the Cursor?
+        if (hideCursor)
+        {
+            // Lock & Hide the Cursor
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+    }
+    // Late Update is called after Update at the end of each frame
+    private void LateUpdate()
+    {
+        // Offsets the Yaw (Y) & Pitch (X) by Mouse X & Y using Speed
+        // Mouse X and Y are Inverted due to Screen Space
+        yaw += Input.GetAxis("Mouse X") * yawSpeed * distance * Time.deltaTime;
+        pitch -= Input.GetAxis("Mouse Y") * pitchSpeed * Time.deltaTime;
+
+        // Clamps the Pitch between Min and Max Limits
+        pitch = ClampAngle(pitch, pitchMinLimit, pitchMaxLimit);
+
+        // Convert Yaw & Pitch to a Quaternion Rotation & Apply Rotation to Transform
+        Quaternion rotation = Quaternion.Euler(pitch, yaw, 0);
+        transform.rotation = rotation;
+
+        // Calculate Distance using Scroll Wheel & Min & Max Distances
+        distance = Mathf.Clamp(distance - Input.GetAxis("Mouse ScrollWheel") * 5, distanceMin, distanceMax);
+
+        // Calculate Position of the Camera using Origin - Camera's Forward x Distance
+        Vector3 origin = target != null ? target.position : Vector3.zero;
+        Vector3 position = origin - transform.forward * distance;
+
+        // Apply Position to Transform
+        transform.position = position;
+    }
+    /// <summary>
+    /// Clamps the given Angle between a given Min & Max Value
+    /// Note: This script confines the angle between 0-360 degrees
+    /// </summary>
+    /// <param name="angle">Angle in Degrees</param>
+    /// <param name="min">Min Angle in Degrees</param>
+    /// <param name="max">Max Angle in Degrees</param>
+    /// <returns></returns>
+    public static float ClampAngle(float angle, float min, float max)
+    {
+        if (angle < -360F)
+            angle += 360F;
+        if (angle > 360F)
+            angle -= 360F;
+        return Mathf.Clamp(angle, min, max);
+    }
+    #endregion
+}
---
+++ Assets/Examples/Cars/Scripts/Car.cs
@@ -0,0 +1,115 @@
+﻿/* Author: Emmanuel (Manny) Vaccaro
+ * Date: 05/03/2020
+ * Brief: This is an example Car class that
+ * outlines the variables & methods associated with every car
+ */
+
+// To use Unity's tools (API), you need this namespace
+using UnityEngine;
+
+/* Background:
+ * Here are some notable keywords used in the following script:
+ * 'access-specifier': public, private, protected
+ * 'class-name': the name you give the class, in this case 'Car'
+ * 'inherited-classes': classes that your class will derive from
+ */
+
+#region Definitions
+public enum Transmission
+{
+    AUTOMATIC = 0,
+    MANUAL = 1
+};
+#endregion
+
+// Classes have the following format: <access-specifier> class <name> : <inherited-classes>
+
+[RequireComponent(typeof(Rigidbody))] // This instruction forces Unity to attach a 'Rigidbody' along with the Car script
+public class Car : MonoBehaviour
+{
+    #region Variables
+    public int value = 4000;            // Value of the car in AUD($)
+    public int windows = 6;             // Amount of windows the car has
+    public int wheels = 4;              // Amount of wheels the car has
+    public float moveSpeed = 10f;       // Movement Speed in km/s
+    public float turnSpeed = 30f;       // Turning Speed in degrees/s
+    public Transmission transmission;   // Transmission: AUTOMATIC / MANUAL
+    private Rigidbody rigid;            // Reference to the rigidbody attached to the same GameObject
+    #endregion
+    #region Functions/Methods
+    #region Unity
+    // Start is called before the first frame update
+    private void Start()
+    {
+        // Rigidbody is marked 'private' so that the script can automatically reference the object
+        rigid = GetComponent<Rigidbody>();
+    }
+    // Update is called once per frame
+    private void Update()
+    {
+        // If the 'W' Key is pressed
+        if (Input.GetKey(KeyCode.W))
+        {
+            // Accelerate the car using speed
+            Accelerate(moveSpeed);
+        }
+
+        // If the 'S' Key is pressed
+        if (Input.GetKey(KeyCode.S))
+        {
+            // Perform Reverse Method
+            Reverse(moveSpeed);
+        }
+
+        // If the 'SpaceBar' is pressed in one frame
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // Stop the car from moving
+            Break();
+        }
+
+        // Get Left / Right input as a float value
+        float inputH = Input.GetAxis("Horizontal");
+        // Steer the Car using horizontal input
+        Steer(inputH);
+    }
+    #endregion
+    /// <summary>
+    /// Accelerates the Car in it's forward direction by a given force value
+    /// </summary>
+    /// <param name="force">Amount of speed to accelerate by</param>
+    public void Accelerate(float force)
+    {
+        // Accelerates the Rigidbody's velocity
+        rigid.AddForce(transform.forward * force);
+    }
+    /// <summary>
+    /// Reverses the Car in the opposite direction of Accelerate by a given force value
+    /// </summary>
+    /// <param name="force">Amount of speed to reverse by</param>
+    public void Reverse(float force)
+    {
+        // Accelerates the Rigidbody's velocity in the opposite direction
+        rigid.AddForce(-transform.forward * force);
+    }
+    /// <summary>
+    /// Stops the car from moving
+    /// </summary>
+    public void Break()
+    {
+        // Cancels out Rigidbody's velocity
+        rigid.velocity = Vector3.zero;
+    }
+    /// <summary>
+    /// Turns the Car's direction by a given angle
+    /// </summary>
+    /// <param name="direction"></param>
+    public void Steer(float direction)
+    {
+        // Force = Direction x Turn Speed
+        float force = direction * turnSpeed;
+        // Apply Torque (Rotational Velocity) in the Up Direction by Force
+        rigid.AddTorque(transform.up * force);
+    }
+    #endregion
+}
